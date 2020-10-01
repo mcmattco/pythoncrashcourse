bcc9d328258d78a3c68244fa19c206a8462a22ff
Matt McClure <56411440+mcmattco@users.noreply.github.com>
2020-10-01 12:03:48 -0400

    ch2, 3

---
+++ ch2/2_vars_tabs_strip.py
@@ -0,0 +1,22 @@
+name = "boB smitH"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "bob"
+last_name = "smith"
+print(first_name + " " + last_name)
+
+full_name = first_name + " " + last_name
+print("Hello " + full_name.title())
+
+print("\nfirst\n\tsecond\nthird")
+
+print("\n")
+
+print("   big space    ".strip())
+
+print("\n")
+
+name = "Fred"
+print("Hello " + name + ", how's it going?")
---
+++ ch2/3_math.py
@@ -0,0 +1,9 @@
+print(2 + 3)
+print(2 ** 3)
+
+age = 37
+print("Happy " + str(age) + "nd birthday!")
+
+print(3 / 2)
+print(3.0 / 2)
+# need float in input to have float in output
