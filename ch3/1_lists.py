363b3c9e16beec744596653d1ebed81ce5cd127c
Matt McClure <56411440+mcmattco@users.noreply.github.com>
2020-10-01 12:57:19 -0400

    Update 1_lists.py

---
+++ ch3/1_lists.py
@@ -0,0 +1,8 @@
+food = ['pizza', 'hot dogs', 'hamburgers', 'cake', 'tacos']
+print(food)
+print(food[0])
+print(food[0].upper())
+print(food[-1])  #last item in list, -2 is next to last, etc.
+
+message = "I love eatting " + food[1] + " and " + food[2] + "."
+print(message)
